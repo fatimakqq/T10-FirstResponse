8ac76498f100224208133d6df83751c39a59c969
George Contreras <georgelcontreras16@gmail.com>
2023-04-14 02:08:49 -0500

    trying to fix charts

--- components/pages/Reports.jsx
+++ components/pages/Reports.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import BarChart from './BarChart';
+import { Bar } from 'react-chartjs-2';
+import {CategoryScale, Chart, registerables} from 'chart.js';
+Chart.register(CategoryScale, registerables);
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [{
+    label: '# of Votes',
+    data: [12, 19, 3, 5, 2, 3],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255, 99, 132, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)'
+    ],
+    borderWidth: 1
+  }]
+}
+
+
 const Reports = () => {
 
   return (
@@ -11,7 +40,7 @@ const Reports = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <BarChart />
+        <Bar data={data} width={400} height={200} options={{maintainAspectRatio: false}}/>
       </IonContent>
     </IonPage>
   );
