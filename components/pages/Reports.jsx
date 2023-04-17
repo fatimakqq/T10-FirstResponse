f8811397b4cbeda67c3e76ea3af8a959659eded5
George Contreras <georgelcontreras16@gmail.com>
2023-04-17 00:01:06 -0500

    2 CHARTS GOT FIXED

--- components/pages/Reports.jsx
+++ components/pages/Reports.jsx
@@ -1,48 +1,129 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { Bar } from 'react-chartjs-2';
-import {CategoryScale, Chart, registerables} from 'chart.js';
-
-const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [{
-    label: '# of Votes',
-    data: [12, 19, 3, 5, 2, 3],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(255, 206, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [
-      'rgba(255, 99, 132, 1)',
-      'rgba(54, 162, 235, 1)',
-      'rgba(255, 206, 86, 1)',
-      'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)'
-    ],
-    borderWidth: 1
-  }]
+import React, { useState, useEffect } from 'react';
+import { IonContent, IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+import Chart from 'chart.js/auto';
+const CallsByType = () =>{
 }
-
-
 const Reports = () => {
-
+  useEffect(() => {
+    //calls by type: bar chart ------------------------------------------------
+    const canvas = document.getElementById('callsByTypeBar');
+    const ctx = canvas.getContext('2d');
+    const data = {
+      labels: ['Trauma', 'Fall', 'Dizziness', 'Injury', 'Intoxication', 'Abdominal Pain', 'Seizure', 'Illness', 'Breathing', 'Syncope'],
+      datasets: [{
+        label: '',
+        data: [12, 4, 3, 8, 2, 3, 4,5,7,2],
+        backgroundColor: [
+            'rgba(255, 159, 64, 0.2)', //orange fill
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(75, 192, 192, 0.2)',//green fill
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(75, 192, 192, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 159, 64, 1)', //orange outline
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(75, 192, 192, 1)',//green outline
+          'rgba(255, 159, 64, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 2
+      }]
+    };
+    const options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      plugins:{
+        legend:{
+            display:false
+        }
+      }
+    };
+    const barChart = new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: options
+    });
+    // response times- line chart ------------------------------------------------
+    const lineCanvas = document.getElementById('responseTimesLine');
+    const lineCtx = lineCanvas.getContext('2d');
+    const lineData = {
+      labels: ['Apr 1-7', 'Apr 8-14', 'Apr 15-21', 'Apr 22-28', 'Apr 29-31'],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [24, 27, 26, 22, 24],
+        fill: false,
+        borderColor: 'rgba(255, 159, 64, 1)',
+        lineTension: 0.1
+      }]
+    };
+    const lineOptions = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      plugins:{
+        legend:{
+        display:false
+        }
+      }
+    };
+    const lineChart = new Chart(lineCtx, {
+      type: 'line',
+      data: lineData,
+      options: lineOptions
+    });
+    //INSERT MORE GRAPHS HERE
+    // Cleanup function to destroy the charts
+    return () => {
+      barChart.destroy();
+      lineChart.destroy();
+    };
+  }, []);
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Bar Graph</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <Bar data={data} width={400} height={200} options={{maintainAspectRatio: false}}/>
-      </IonContent>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle className= "font-majorMonoDisplay">Reports</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+            <div className="flex mt-5 text-gray-400 items-center justify-center">
+                <select className="bg-transparent  font-manjari border-none "interface="popover"
+                placeholder="Last 30 days">
+                    <option value="month" selected>Last 30 days</option>
+                    <option value="week">Last 1 week</option>
+                    <option value="year">Last 1 year</option>
+                </select>
+            </div>
+            <h1 className='font-manjari  mb-0 flex items-center justify-center' >Calls By Type</h1>
+            <canvas id="callsByTypeBar"></canvas>
+            <h1 className='  font-manjari  mb-0 flex items-center justify-center' >Response Times</h1>
+            <canvas id="responseTimesLine"></canvas>
+            <h1 className='font-manjari  mb-0 flex items-center justify-center' >Calls By Shift -bar</h1>
+            <h1 className='font-manjari  mb-0 flex items-center justify-center' >Calls By Disposition -pie</h1>
+            <h1 className='font-manjari  mb-0 flex items-center justify-center' >Calls By Day of Week -bar</h1>
+            <h1 className='font-manjari  mb-0 flex items-center justify-center' >Patients By Type -bar</h1>
+        </IonContent>
     </IonPage>
   );
-};
-
+}
 export default Reports;
