77b58befa5e7a505c5cd367a9f796703e4cf0fd6
Shannon Carter <shannon.m.carter.18@gmail.com>
2023-04-18 20:47:47 -0500

    updated index.js

--- components/pages/index.js
+++ components/pages/index.js
@@ -1,23 +1,11 @@
 
 export const emLogInfo = [
-<<<<<<< HEAD
-    { logId: "1", isActive: "true", date: "2/20/23", location: "ECSS 2.415", title: "Head Injury", timeStart: "1:02PM", timeEnd: "1:34PM", time: "1:02PM - 1:34PM" },
-    { logId: "2", isActive: "true", date: "2/20/23", location: "SU", title: "Seizure", timeStart: "7:40PM", timeEnd: "8:25PM", time: "7:40PM - 8:25PM" },
-    { logId: "3", isActive: "true", date: "2/20/23", location: "SCI 1.210", title: "Fainting/Unconscious", timeStart: "11:27PM", timeEnd: "12:08AM", time: "11:27PM - 12:08AM" },
-
-    { logId: "4", isActive: "true", date: "2/19/23", location: "ECSS 2.415", title: "Traumatic Injury", timeStart: "1:47PM", timeEnd: "2:00PM" },
-    { logId: "5", isActive: "true", date: "2/19/23", location: "GR 2.220", title: "Respiratory Distress", timeStart: "2:49PM", timeEnd: "3:11PM" },
-    { logId: "6", isActive: "true", date: "2/17/23", location: "SU", title: "Seizure", timeStart: "11:34PM", timeEnd: "12:08AM" },
-    { logId: "7", isActive: "true", date: "2/16/23", location: "McDermott L.", title: "Behavioral Disorder", timeStart: "3:02PM", timeEnd: "3:30PM" },
-    { logId: "8", isActive: "true", date: "2/16/23", location: "SCI 3.707", title: "Abdominal Pain", timeStart: "7:54AM", timeEnd: "8:12AM" },
-    { logId: "9", isActive: "true", date: "2/15/23", location: "SLC 1.222", title: "Fainting/Unconscious", timeStart: "4:44PM", timeEnd: "5:24PM" },
+    {  logId: "1", isActive: "true", date: "4/26/23", location: "ECSS 2.415", mapLocation: "Erik Jonsson School of Engineering and Computer Science, ECSS", title: "Head Injury", timeStart: "1:02PM", timeEnd: "ACTIVE", time: "1:02 - 1:34PM", lat:"-25.42986",lng:"-66.56283", info: "21 year old male" },
+    {  logId: "2", isActive: "true", date: "4/24/23", location: "SU", mapLocation: "8645X7P2+PC", title: "Seizure", timeStart: "7:40", timeEnd: "8:25PM", time: "7:40 - 8:25PM", lat:null,lng:null, info: "18 year old female" },
+    {  logId: "3", isActive: "true", date: "4/23/23", location: "SCI 1.210", mapLocation: "Sciences Building, Richardson",title: "Fainting/Unconscious", timeStart: "11:27", timeEnd: "12:08AM", time: "11:27 - 12:08AM",lat:null,lng:null, info: "47 year old male" },
 ]; //end of emLogInfo array
 
-=======
-  {  logId: "1", isActive: "true", date: "4/26/23", location: "ECSS 2.415", mapLocation: "Erik Jonsson School of Engineering and Computer Science, ECSS", title: "Head Injury", timeStart: "1:02PM", timeEnd: "ACTIVE", time: "1:02 - 1:34PM", lat:"-25.42986",lng:"-66.56283", info: "21 year old male" },
-  {  logId: "2", isActive: "true", date: "4/24/23", location: "SU", mapLocation: "8645X7P2+PC", title: "Seizure", timeStart: "7:40", timeEnd: "8:25PM", time: "7:40 - 8:25PM", lat:null,lng:null, info: "18 year old female" },
-  {  logId: "3", isActive: "true", date: "4/23/23", location: "SCI 1.210", mapLocation: "Sciences Building, Richardson",title: "Fainting/Unconscious", timeStart: "11:27", timeEnd: "12:08AM", time: "11:27 - 12:08AM",lat:null,lng:null, info: "47 year old male" },
-  ]; //end of emLogInfo array
+
 
 /*
   export async function updateCoordinates() {
@@ -42,7 +30,6 @@ export const notifications = [
   { title: 'You have a new message', when: '2 weeks' },
   { title: 'Welcome to the app!', when: '1 month' }, 
 ];
->>>>>>> 335bcdb36d27416278aacf005e963e7b55d0f20e
 
 export const calendarInfo = [
     { shiftID: "1", date: "4/26/23", timeStart: "8AM", timeEnd: "12PM", time: "8AM - 12PM" },
